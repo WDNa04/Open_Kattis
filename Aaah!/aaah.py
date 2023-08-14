94aa71de19264d7ff77c97a0f3b622df1f0da83b
WDNa04 <WNa0531@outlook.com>
2023-08-14 18:01:13 +0800

    hi

---
+++ Aaah!/aaah.py
@@ -0,0 +1,10 @@
+import sys
+
+Jon = sys.stdin.readline()
+doctor = sys.stdin.readline()
+Jon_count = Jon.count('a')
+doctor_count = doctor.count('a')
+if doctor_count <= Jon_count:
+    print("go")
+else:
+    print("no")
