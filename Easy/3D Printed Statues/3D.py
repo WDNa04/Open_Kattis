41ee3571d0cb3f8351dfde3b493cf082a6b4842f
WDNa04 <WNa0531@outlook.com>
2023-08-20 12:24:41 +0800

    hi

--- Easy/3D Printed Statues/3D.py
+++ Easy/3D Printed Statues/3D.py
@@ -1,1 +1,14 @@
-pip install flet
+import sys
+
+count = int(sys.stdin.readline())
+
+i = 0
+while True:
+    if count == 1:
+        print(1)
+        break
+    elif count >= 2 ** i + 1 and count <= 2 ** (i+1):
+        print(i + 2)
+        break
+    else:
+        i += 1
